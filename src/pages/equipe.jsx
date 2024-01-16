6490e9e05782cb7758af48752a41fd59a1cf1496
André Marques <andre.m.d.abreu@gmail.com>
2024-01-16 12:01:25 -0300

    Criação de páginas e links

---
+++ src/pages/equipe.jsx
@@ -0,0 +1,14 @@
+import Menu from "@/components/Menu";
+
+export default function Equipe() {
+  return (
+    <>
+      <h1>Olá Next.js!</h1>
+      <Menu />
+      <main>
+        <h2>Equipe</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt praesentium magnam dolore eveniet quasi, quo mollitia tempora reiciendis officiis sapiente. Reiciendis maxime error aperiam provident dignissimos culpa veniam est minus.</p>
+      </main>
+    </>
+  );
+}
