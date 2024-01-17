f1652b9f26d46c3610f8b9976a1d4dace9ca7fc1
André Marques <andre.m.d.abreu@gmail.com>
2024-01-17 09:10:01 -0300

    Adicionando tag Main no _app.js e metatag no _document.js

--- src/pages/equipe.jsx
+++ src/pages/equipe.jsx
@@ -1,14 +1,21 @@
 import Menu from "@/components/Menu";
+import Head from "next/head";
 
 export default function Equipe() {
   return (
     <>
+      <Head>
+        <title>Equipe</title>
+      </Head>
       <h1>Olá Next.js!</h1>
       <Menu />
-      <main>
-        <h2>Equipe</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt praesentium magnam dolore eveniet quasi, quo mollitia tempora reiciendis officiis sapiente. Reiciendis maxime error aperiam provident dignissimos culpa veniam est minus.</p>
-      </main>
+      <h2>Equipe</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+        praesentium magnam dolore eveniet quasi, quo mollitia tempora reiciendis
+        officiis sapiente. Reiciendis maxime error aperiam provident dignissimos
+        culpa veniam est minus.
+      </p>
     </>
   );
 }
--- src/pages/servicos.jsx
+++ src/pages/servicos.jsx
@@ -1,18 +1,20 @@
 import Menu from "@/components/Menu";
+import Head from "next/head";
 
 export default function Servicos() {
   return (
     <>
+      <Head>
+        <title>Serviços</title>
+      </Head>
       <h1>Olá Next.js!</h1>
       <Menu />
-      <main>
-        <h2>Serviços</h2>
-        <ol>
-          <li>Bla bla bla</li>
-          <li>Bla bla bla</li>
-          <li>Bla bla bla</li>
-        </ol>
-      </main>
+      <h2>Serviços</h2>
+      <ol>
+        <li>Bla bla bla</li>
+        <li>Bla bla bla</li>
+        <li>Bla bla bla</li>
+      </ol>
     </>
   );
 }
